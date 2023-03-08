492f86154a1ab52bbc86c08b588595697b3679cc
brunopc-net <work@brunopc.net>
2023-03-08 16:08:25 -0500

    Hasher

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ log = log4p.GetLogger(__name__, config="log4p.json").logger
 if __name__ == '__main__':
     log.info(os.listdir("/home/bruno"))
     documents_hash = str(hasher.hash_blake2b(directory='documents'))
+    log.debug("Current documents hash: " + documents_hash)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
