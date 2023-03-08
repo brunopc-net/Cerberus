708a13001b4e8a3efe98df8d9339b8bfd720b3ed
brunopc-net <work@brunopc.net>
2023-03-08 16:04:09 -0500

    Hasher

---
+++ src/_hasher.py
@@ -0,0 +1,20 @@
+import hashlib
+
+from pathlib import Path
+
+
+def hash_blake2b(directory):
+    return hash_dir(directory, hashlib.blake2b())
+
+
+def hash_dir(directory, hasher):
+    assert Path(directory).is_dir()
+    for path in sorted(Path(directory).iterdir(), key=lambda p: str(p).lower()):
+        hasher.update(path.name.encode())
+        if path.is_file():
+            with open(path, "rb") as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hasher.update(chunk)
+        elif path.is_dir():
+            hasher = hasher(path, hasher)
+    return hash.hexdigest()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
-# This is a sample Python script.
 import os
-
 import log4p
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src import _hasher as hasher
 
 log = log4p.GetLogger(__name__, config="log4p.json").logger
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     log.info(os.listdir("/home/bruno"))
+    documents_hash = str(hasher.hash_blake2b(directory='documents'))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
