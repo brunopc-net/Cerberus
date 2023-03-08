33e1719b03449d6294e40788567c5a83c807a8a1
brunopc-net <work@brunopc.net>
2023-03-08 13:24:42 -0500

    Log4p

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
 # This is a sample Python script.
 import os
 
+import log4p
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+log = log4p.GetLogger(__name__, config="log4p.json").logger
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(os.listdir("/home/bruno"))
+    log.info(os.listdir("/home/bruno"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
