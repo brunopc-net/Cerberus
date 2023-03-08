ab370fe4f6bfbe602b23d748644720c701d53f39
brunopc-net <work@brunopc.net>
2023-03-08 15:16:24 -0500

    Success

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ log = log4p.GetLogger(__name__, config="log4p.json").logger
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(os.listdir("/home/bruno"))
     log.info(os.listdir("/home/bruno"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
