9fea35d2f948af4df7ba4bd34878f12e3ba5d10b
brunopc-net <work@brunopc.net>
2023-03-09 17:12:33 -0500

    bug fix

--- src/_hasher.py
+++ src/_hasher.py
@@ -4,18 +4,15 @@ import log4p
 from pathlib import Path
 
 log = log4p.GetLogger(__name__, config="log4p.json").logger
-
 def get_hash_blake2b(directory):
-    return get_hash_value(directory, hashlib.blake2b)
-
+    return hash_dir(directory, hashlib.blake2b())
 
 def get_hash_value(directory, hasher):
-    hash_value = str(get_hash(directory, hasher))
+    hash_value = str(hash_dir(directory, hasher).hexdigest())
     log.debug("Current hash for the directory %s: %s", directory, hash_value)
     return hash_value
 
-
-def get_hash(directory, hasher):
+def hash_dir(directory, hasher):
     assert Path(directory).is_dir()
     for path in sorted(Path(directory).iterdir(), key=lambda p: str(p).lower()):
         hasher.update(path.name.encode())
@@ -24,6 +21,5 @@ def get_hash(directory, hasher):
                 for chunk in iter(lambda: f.read(4096), b""):
                     hasher.update(chunk)
         elif path.is_dir():
-            hasher = get_hash(path, hasher)
-    return hasher.hexdigest()
-
+            hasher = hash_dir(path, hasher)
+    return hasher
