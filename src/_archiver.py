a691069402d1f8ebe724f494e2d8964fb015612e
brunopc-net <work@brunopc.net>
2023-03-10 21:44:07 -0500

    Archive documents

--- src/_archiver.py
+++ src/_archiver.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 
 def archive(directory):
-    tar_filename = directory + datetime.today().strftime('%Y-%m-%d') + '.tar'
+    tar_filename = datetime.today().strftime('%Y-%m-%d') + '.tar'
     with tarfile.open(tar_filename, "w") as archive_file:
         for root, dirs, files in os.walk(directory):
             for f in files:
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import hashlib
 import redis
 
 import _hasher as hasher
+import _archiver as archiver
 
 from pathlib import Path
 
@@ -13,7 +14,7 @@ redis = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 def is_backup_necessary(directory_path):
     current_h = hasher.get_hash(directory_path, hashlib.blake2b())
-    log.debug("Current hash for directory content: %s ",current_h)
+    log.debug("Current hash for directory content: %s ", current_h)
 
     hash_key = "hash_" + directory_path
     previous_h = str(redis.get(hash_key))
@@ -30,8 +31,10 @@ if __name__ == '__main__':
     dir_name = sys.argv[1]
     log.info("Launching backup procedure for directory %s", dir_name)
     assert Path(dir_name).is_dir()
-    to_backup = is_backup_necessary(dir_name)
-    log.info("Need to backup directory: %s", str(to_backup))
+    archiver.archive(dir_name)
+    #to_backup = is_backup_necessary(dir_name)
+    #log.info("Need to backup directory: %s", str(to_backup))
+
 
 
 
