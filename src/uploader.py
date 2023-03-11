6d132ba3c5ea0e11f976eec5037ff1c19165c4eb
brunopc-net <work@brunopc.net>
2023-03-11 18:03:25 -0500

    Running tests in pipeline

--- src/uploader.py
+++ src/uploader.py
@@ -4,14 +4,20 @@ import pcloud
 
 log = log4p.GetLogger(__name__, config="log4p.json").logger
 
+PATH = '/Bruno/'
 
-def upload(file, username, password):
-    pcloud_path = '/Bruno/' + file
-    log.info('Uploading %s file to pcloud %s', pcloud_path)
-    response = pcloud.PyCloud(username, password).uploadfile(files=[file], path=pcloud_path)
 
-    if response['result'] == 0:
-        log.info('Uploading to pcloud: Success!')
-        return
-    log.error("Was not able to upload to pcloud: " + str(response))
-    sys.exit(response['result'])
+class Uploader:
+
+    def __init__(self, username, password):
+        self.pc = pcloud.PyCloud(username, password)
+
+    def upload(self, file):
+        path = PATH + str(file)
+        log.info('Uploading %s file to pcloud %s', file, path)
+        response = self.pc.uploadfile(files=[file], path=path)
+        if response['result'] == 0:
+            log.info('Uploading to pcloud: Success!')
+            return
+        log.error("Was not able to upload to pcloud: " + str(response))
+        sys.exit(response['result'])
--- tests/uploader_test.py
+++ tests/uploader_test.py
@@ -4,12 +4,8 @@ import src.uploader as uploader
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_upload_fail(self):
-        f = open("test_upload.txt", "w")
-        f.write("some data")
-        f.close()
-
+    def test_login_fail(self):
         self.assertRaises(
             pcloud.api.AuthenticationError,
-            uploader.upload('bad_user', 'bad_password', f)
+            uploader.Uploader, "bad_user", "bad_password"
         )
