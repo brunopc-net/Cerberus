b134f7f75431b2eef89880e0559625e338b20faa
brunopc-net <work@brunopc.net>
2023-03-11 17:13:29 -0500

    Running tests in pipeline

--- tests/archiver_test.py
+++ tests/archiver_test.py
@@ -1,12 +1,11 @@
 # from https://github.com/taisei-project/python-zipfile-zstd
 import os
 import unittest
-
-from .context import src
+import archiver
 
 
 class TestStringMethods(unittest.TestCase):
     def test_archive(self):
-        archive_file = src.archive("./")
+        archive_file = archiver.archive("./")
         self.assertTrue(os.path.isfile(archive_file))
         os.remove(archive_file)
