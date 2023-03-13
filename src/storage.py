dce57e230f5eb386749d0c32bf1ede406b71bead
brunopc-net <work@brunopc.net>
2023-03-13 19:27:02 -0400

    Refactoring - storage resource

---
+++ src/storage.py
@@ -0,0 +1,34 @@
+import os
+import redis
+import log4p
+
+import date
+
+log = log4p.GetLogger(__name__, config="log4p.json").logger
+redis = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+
+LAST_EXECUTION_KEY = "last_successful_execution"
+
+
+def get_last_execution_date():
+    last_execution_date = redis.get(LAST_EXECUTION_KEY)
+    log.debug("Last execution date: %s", last_execution_date)
+    return last_execution_date
+
+
+def update_last_execution_date():
+    redis.set(LAST_EXECUTION_KEY, date.get_today())
+
+
+def get_last_directory_hash(directory_path):
+    last_hash = str(redis.get(get_hash_key(directory_path)))
+    log.debug("Previous hash: %s", last_hash)
+    return last_hash
+
+
+def update_directory_hash(directory_path, new_hash):
+    redis.set(get_hash_key(directory_path), new_hash)
+
+
+def get_hash_key(directory_path):
+    return "hash_" + os.path.dirname(directory_path)
---
+++ tests/storage_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+import src.storage as storage
+
+
+class StorageTest(unittest.TestCase):
+    def test_store_last_execution_date(self):
+        storage.update_last_execution_date()
+        self.assertIsNotNone(storage.get_last_execution_date())
+
+    def test_store_last_hash_directory_hash(self):
+        fictive_directory_name = "/fictive_directory"
+        fictive_directory_hash = "123456"
+        storage.update_directory_hash(fictive_directory_name, fictive_directory_hash)
+        self.assertEquals(
+            fictive_directory_hash,
+            storage.get_last_directory_hash(fictive_directory_name)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
