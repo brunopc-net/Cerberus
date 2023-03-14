87c213be335c28361d60c81ddf2c6ae5aa9d8682
brunopc-net <work@brunopc.net>
2023-03-14 16:34:43 -0400

    test fix

--- tests/archiver_test.py
+++ tests/archiver_test.py
@@ -18,7 +18,7 @@ class ArchiverTest(unittest.TestCase):
         os.mkdir(directory)
         self.assertTrue(archiver.is_backup_needed(directory))
 
-        storage.update_directory_hash(directory, hasher.get_directory_hash(directory, hashlib.blake2b))
+        storage.update_directory_hash(directory, hasher.get_directory_hash(directory, hashlib.blake2b()))
         self.assertFalse(archiver.is_backup_needed(directory))
 
         storage.delete_directory_hash(directory)
