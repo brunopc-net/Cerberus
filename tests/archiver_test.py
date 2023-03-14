0a9bf6765c1c7ba6a9db264b0b3ee8b37f687c29
brunopc-net <work@brunopc.net>
2023-03-14 13:16:04 -0400

    Fixing test_is_backup_needed assertion error

--- tests/archiver_test.py
+++ tests/archiver_test.py
@@ -10,18 +10,18 @@ import src.archiver as archiver
 
 class ArchiverTest(unittest.TestCase):
     def test_archive(self):
-        archive_file = archiver.archive("./")
+        archive_file = archiver.archive("./src")
         self.assertTrue(os.path.isfile(archive_file))
         os.remove(archive_file)
 
     def test_is_backup_needed(self):
-        self.assertTrue(archiver.is_backup_needed("/src"))
-        hasher.get_directory_hash("/src", hashlib.blake2b())
-        hasher.get_directory_hash("/src", hashlib.blake2b())
-        hasher.get_directory_hash("/src", hashlib.blake2b())
+        self.assertTrue(archiver.is_backup_needed("./src"))
+        hasher.get_directory_hash("./src", hashlib.blake2b())
+        hasher.get_directory_hash("./src", hashlib.blake2b())
+        hasher.get_directory_hash("./src", hashlib.blake2b())
 
-        storage.update_directory_hash("/src", hasher.get_directory_hash("/src", hashlib.blake2b()))
-        self.assertFalse(archiver.is_backup_needed("/src"))
+        storage.update_directory_hash("./src", hasher.get_directory_hash("./src", hashlib.blake2b()))
+        self.assertFalse(archiver.is_backup_needed("./src"))
 
 
 if __name__ == "__main__":
